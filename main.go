bf6efdc5ef9e2d4c1ef686126412f85fed57e187
Jeff McCune <jeff@openinfrastructure.co>
2024-12-26 16:58:02 -0800

    decent, around 11.5 seconds
    Produce work in 4 MiB chunks.  The producer takes care to align the
    buffer, ensuring it always ends in a newline for the consumers.
    
    Maxes out the CPU, scanning lines definitely did not.
    
    ./1brc-go < measurements-big.txt  138.50s user 5.66s system 1264% cpu 11.405 total

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"context"
 	"flag"
 	"fmt"
@@ -11,7 +11,6 @@ import (
 	"runtime/pprof"
 	"sort"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -19,30 +18,103 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-type measurement struct {
-	mu    *sync.Mutex
-	min   float64
-	max   float64
-	total float64
-	count int
+type Task interface {
+	Run(ctx context.Context) (StationMap, error)
 }
 
-func (m *measurement) Update(temperature float64) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.min = min(m.min, temperature)
-	m.max = max(m.max, temperature)
-	m.total += temperature
-	m.count += 1
+var _ Task = &TaskV1{}
+
+type TaskV1 struct {
+	id  int
+	buf []byte
 }
 
-func NewMeasurement(temperature float64) *measurement {
-	return &measurement{
-		mu:    &sync.Mutex{},
-		min:   temperature,
-		max:   temperature,
-		total: temperature,
-		count: 1,
+func (r *TaskV1) Run(ctx context.Context) (StationMap, error) {
+	sm := make(StationMap, 10_000)
+	buf := bytes.NewBuffer(r.buf)
+
+	for {
+		line, err := buf.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				return sm, nil
+			}
+			return sm, errors.Wrap(err)
+		}
+		// split into station name and measurement
+		idx := bytes.Index(line, []byte(";"))
+		station := string(line[:idx])
+		// Parse the integer part, skip the newline at the end.
+		temp, err := strconv.ParseInt(string(line[idx+1:len(line)-3]), 10, 16)
+		if err != nil {
+			return sm, errors.Wrap(err)
+		}
+		// Parse the fractional part, skip the newline at the end.
+		fractional, err := strconv.ParseUint(string(line[len(line)-2]), 10, 16)
+		if err != nil {
+			return sm, errors.Wrap(err)
+		}
+		// the final temperature multiplied by 10
+		if temp > 0 {
+			temp = temp*10 + int64(fractional)
+		} else {
+			temp = temp*10 - int64(fractional)
+		}
+
+		// Update the station map.
+		sm.Add(station, temp)
+	}
+}
+
+type Temperature struct {
+	min, max, sum, num int64
+}
+
+func (t *Temperature) Add(val int64) {
+	t.min = min(t.min, val)
+	t.max = max(t.max, val)
+	t.sum += val
+	t.num += 1
+}
+
+func (t *Temperature) Merge(other *Temperature) {
+	t.min = min(t.min, other.min)
+	t.max = max(t.max, other.max)
+	t.sum += other.sum
+	t.num += other.num
+}
+
+func (t Temperature) String() string {
+	return fmt.Sprintf(
+		"%.1f/%.1f/%.1f",
+		0.1*float64(t.min),
+		0.1*float64(t.sum/t.num),
+		0.1*float64(t.max),
+	)
+}
+
+type StationMap map[string]*Temperature
+
+func (sm StationMap) Merge(other StationMap) {
+	for station, temp := range other {
+		if t, ok := sm[station]; ok {
+			t.Merge(temp)
+		} else {
+			sm[station] = temp
+		}
+	}
+}
+
+func (sm StationMap) Add(station string, val int64) {
+	if t, ok := sm[station]; ok {
+		t.Add(val)
+	} else {
+		sm[station] = &Temperature{
+			min: val,
+			max: val,
+			sum: val,
+			num: 1,
+		}
 	}
 }
 
@@ -69,95 +141,153 @@ func main() {
 }
 
 func Run(ctx context.Context, r io.Reader) error {
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Minute)
+	ctx, cancel := context.WithTimeout(ctx, 20*time.Second)
 	defer cancel()
 	g, ctx := errgroup.WithContext(ctx)
-	tasks := make(chan string)
 
-	syncMap := sync.Map{}
+	tasks := make(chan Task)
+	results := make(chan StationMap)
 
 	// Consumers
-	for i := 0; i < runtime.NumCPU(); i++ {
-		g.Go(func() error { return Consumer(ctx, tasks, &syncMap) })
+	var wg sync.WaitGroup
+	for id := 0; id < runtime.NumCPU(); id++ {
+		wg.Add(1)
+		g.Go(func() error {
+			defer wg.Done()
+			return Consumer(ctx, id, tasks, results)
+		})
 	}
 
 	// Producer
 	g.Go(func() error { return Producer(ctx, tasks, r) })
 
+	// Collector
+	stationMap := make(StationMap, 10_000)
+	g.Go(func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return errors.Wrap(ctx.Err())
+			case sm, ok := <-results:
+				if !ok {
+					return nil
+				}
+				stationMap.Merge(sm)
+			}
+		}
+	})
+
+	// Release the collector once all consumers have submitted results.
+	g.Go(func() error {
+		wg.Wait()
+		close(results)
+		return nil
+	})
+
 	if err := g.Wait(); err != nil {
 		return errors.Wrap(err)
 	}
 
-	if err := Summary(ctx, &syncMap, os.Stdout); err != nil {
+	if err := Summary(ctx, stationMap, os.Stdout); err != nil {
 		return errors.Wrap(err)
 	}
 
 	return g.Wait()
 }
 
-func Consumer(ctx context.Context, tasks chan string, syncMap *sync.Map) error {
+func Consumer(ctx context.Context, id int, tasks chan Task, results chan StationMap) error {
 	for {
 		select {
 		case <-ctx.Done():
 			return errors.Wrap(ctx.Err())
-		case s, ok := <-tasks:
+		case task, ok := <-tasks:
 			if !ok {
 				// producer closed the tasks channel, stop the consumer.
 				return nil
 			}
-			rec := strings.SplitN(s, ";", 2)
-			if len(rec) != 2 {
-				continue // invalid record, can happen on the final newline.
-			}
-			temp, err := strconv.ParseFloat(rec[1], 64)
+			sm, err := task.Run(ctx)
 			if err != nil {
 				return errors.Wrap(err)
 			}
-
-			m := NewMeasurement(temp)
-			val, loaded := syncMap.LoadOrStore(rec[0], m)
-			if loaded {
-				m = val.(*measurement)
-				m.Update(temp)
+			select {
+			case <-ctx.Done():
+				return errors.Wrap(ctx.Err())
+			case results <- sm:
 			}
 		}
 	}
 }
 
-func Producer(ctx context.Context, tasks chan string, r io.Reader) error {
-	defer close(tasks)
+// ReadChunk reads a size chunk of data, with head and tail split on the last
+// newline.
+func ReadChunk(partial []byte, size int, r io.Reader) (head, tail []byte, err error) {
+	buf := make([]byte, size)
+	n, err := r.Read(buf)
+	if n == 0 {
+		return nil, partial, err
+	}
 
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
+	combined := partial
+	combined = append(combined, buf[:n]...)
+
+	idx := bytes.LastIndexByte(combined, '\n')
+
+	if idx == -1 {
+		return nil, combined, errors.Format("no newline found in %d bytes", len(combined))
+	}
+
+	head = combined[:idx+1]
+	tail = combined[idx+1:]
+	return
+}
+
+// Producer produces tasks with complete buffers ending in a newline.
+func Producer(ctx context.Context, tasks chan Task, r io.Reader) error {
+	defer close(tasks)
+	var id int
+	var tail []byte
+
+	for {
+		head, newTail, err := ReadChunk(tail, 1024*1024*4, os.Stdin)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
 		select {
-		case tasks <- scanner.Text():
+		case tasks <- &TaskV1{id: id, buf: head}:
+		case <-ctx.Done():
+			return errors.Wrap(ctx.Err())
+		}
+		tail = newTail
+		id += 1
+	}
+
+	// Handle remaining data
+	if len(tail) > 0 {
+		select {
+		case tasks <- &TaskV1{buf: tail}:
 		case <-ctx.Done():
 			return errors.Wrap(ctx.Err())
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		return errors.Wrap(err)
-	}
+
 	return nil
 }
 
 // emit the results on stdout like this (i.e. sorted alphabetically by station
 // name, and the result values per station in the format <min>/<mean>/<max>,
 // rounded to one fractional digit)
-func Summary(ctx context.Context, syncMap *sync.Map, w io.Writer) error {
+func Summary(ctx context.Context, sm StationMap, w io.Writer) error {
 	stations := make([]string, 0, 10000)
-	syncMap.Range(func(station, v any) bool {
-		stations = append(stations, station.(string))
-		return true
-	})
+	for station := range sm {
+		stations = append(stations, station)
+	}
 	sort.Strings(stations)
 	for _, station := range stations {
-		v, ok := syncMap.Load(station)
-		if !ok {
-			return errors.Format("station %s not found", station)
-		}
-		m := v.(*measurement)
-		fmt.Fprintf(w, "%s=%.1f/%.1f/%.1f\n", station, m.min, m.total/float64(m.count), m.max)
+		temperature, _ := sm[station]
+		fmt.Fprintf(w, "%s=%s\n", station, temperature.String())
 	}
 	return nil
 }
